28e6973044e00c9fdef0fb420a685b98257fad2e
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-07-02 15:11:13 +0800

    refactor: renamed from ToDo to Login

--- src/main/java/com/shyrine/todolist/todolist/Login.java
+++ src/main/java/com/shyrine/todolist/todolist/Login.java
@@ -1,20 +1,28 @@
 package com.shyrine.todolist.todolist;
 
+import com.shyrine.todolist.todolist.util.SQLiteConnectionUtil;
 import com.shyrine.todolist.todolist.util.SceneUtils;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class ToDo extends Application {
+public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Scene scene = SceneUtils.createScene("com/shyrine/todolist/todolist/todo-view.fxml", 960, 540);
+        SQLiteConnectionUtil.getConnection();
+        Scene scene = SceneUtils.createScene("com/shyrine/todolist/todolist/login.fxml", 960, 540);
 
         stage.setTitle("ToDoList by Shyrine");
         stage.getIcons().add(SceneUtils.loadApplicationIcon());
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+    }
+
+    @Override
+    public void stop() {
+        SQLiteConnectionUtil.closeConnection();
     }
 
     public static void main(String[] args) {
