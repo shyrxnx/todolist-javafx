ad5fad4288c905c2c40ce58331e94e4e6b251347
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-07-02 15:12:48 +0800

    update: add java.sql module dependency in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.shyrine.todolist.todolist {
     requires javafx.fxml;
 
     requires org.kordamp.ikonli.javafx;
+    requires java.sql;
 
     opens com.shyrine.todolist.todolist to javafx.fxml;
     exports com.shyrine.todolist.todolist;
