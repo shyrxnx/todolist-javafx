1a25dfa74ef041a1677622211f5c88255c87f290
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-07-02 11:09:24 +0800

    refactor: utilize SceneUtils for scene creation and icon loading in ToDo class

--- src/main/java/com/shyrine/todolist/todolist/ToDo.java
+++ src/main/java/com/shyrine/todolist/todolist/ToDo.java
@@ -1,23 +1,18 @@
 package com.shyrine.todolist.todolist;
 
+import com.shyrine.todolist.todolist.util.SceneUtils;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Objects;
 
 public class ToDo extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ToDo.class.getResource("todo-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
-        Image appIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/shyrine/todolist/todolist/images/todo_list_logo.png")));
+        Scene scene = SceneUtils.createScene("com/shyrine/todolist/todolist/todo-view.fxml", 960, 540);
 
         stage.setTitle("ToDoList by Shyrine");
-        stage.getIcons().add(appIcon);
+        stage.getIcons().add(SceneUtils.loadApplicationIcon());
         stage.setScene(scene);
         stage.show();
     }
